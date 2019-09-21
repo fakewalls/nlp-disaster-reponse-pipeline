14d8a59526f3882e5e15f7a842439edd426d8837
fakewalls <thenjunderground@gmail.com>
2019-09-21 21:05:56 +0000

    process data ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,33 @@
 import sys
-
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+    return df
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(pat=';', expand=True)
 
+    row = categories.loc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        categories[column] = pd.to_numeric(categories[column])
 
+    df = df.drop('categories', axis=1)
+    df2 = pd.concat([df, categories], axis=1)
+    df2 = df2.drop_duplicates()
+    return df2
+    
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///data/DisasterResponse.db')
+    df.to_sql('MessagesCategoriesClean', engine, index=False)
 
 
 def main():

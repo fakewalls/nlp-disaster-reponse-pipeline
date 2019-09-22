f38cf9b475d7e5565e677a8582b2a62084a257b8
fakewalls <thenjunderground@gmail.com>
2019-09-22 22:59:55 +0000

    1) load from db. 2) Tokenize. 3) Train model. 4) Evaluate model. 5) Save as pickel.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,41 +1,98 @@
 import sys
+import numpy as np
+import pandas as pd
+import pickle
+import sqlite3
+import nltk
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_query("select * from MessagesCategoriesClean;", engine)
+    X = df['message'].values
+    y = df[['related', 'request', 'offer',
+           'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+           'security', 'military', 'child_alone', 'water', 'food', 'shelter',
+           'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid',
+           'infrastructure_related', 'transport', 'buildings', 'electricity',
+           'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+           'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+           'other_weather', 'direct_report']].values
+    category_names = df[['related', 'request', 'offer',
+           'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+           'security', 'military', 'child_alone', 'water', 'food', 'shelter',
+           'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid',
+           'infrastructure_related', 'transport', 'buildings', 'electricity',
+           'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+           'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+           'other_weather', 'direct_report']].columns.tolist()
 
+    return X, y, category_names
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_jobs=-1)))
+        ])
+    parameters = {
+        'clf__estimator__max_depth': (None, 1),
+        'clf__estimator__min_samples_split': (2, 3)
+    }
 
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+def evaluate_model(model, X_test, y_test, category_names):
+    y_pred = model.predict(X_test)
+    for i, column in enumerate(category_names):
+        print(column)
+        print(classification_report([y[i] for y in y_test], [y[i] for y in y_pred]))
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         
         print('Building model...')
         model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train, y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
